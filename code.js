e80479aae7b9bc61861a1a6918c889a5546c26f0
Luis Corrales Marin <luiscorrales5012@gmail.com>
2021-08-26 20:44:26 -0500

    Refactorización codigo

--- code.js
+++ code.js
@@ -1,6 +1,80 @@
+import {vestidos, faldas, jeans, tennis, zandalias} from './data/bdVestidos.js'
+
 const nav = document.querySelector('.content-span button');
 const menu = document.querySelector('.content-menu');
+const gridVestidos = document.querySelector('.grid-vestidos');
+const girdFaldas = document.querySelector('.grid-faldas');
+const girdJeans = document.querySelector('.grid-jeans');
+const girdTennis = document.querySelector('.grid-tennis');
+const girdZandalias = document.querySelector('.grid-zapato-zandalia');
+
+
 nav.addEventListener('click', e=>{
     nav.classList.toggle('open');
     menu.classList.toggle('content-menu-open');
+});
+
+vestidos.forEach(vestido => {
+    const {src,nombre,precio,href,boton} = vestido
+    gridVestidos.innerHTML += `
+    <div class="grid-vestidos__items">
+        <img src=${src} alt="">
+        <h2><span>${nombre}</span><br>${Math.round(Math.random()*precio)}</h2>
+        <a href=${href}>
+            <button>${boton}</button>
+        </a>
+    </div>
+    `
+});
+
+faldas.forEach(falda => {
+    const {src,nombre,precio,href,boton} = falda
+    girdFaldas.innerHTML += `
+    <div class="grid-vestidos__items">
+        <img src=${src} alt="">
+        <h2><span>${nombre}</span><br>${Math.round(Math.random()*precio)}</h2>
+        <a href=${href}>
+            <button>${boton}</button>
+        </a>
+    </div>
+    `
+});
+
+jeans.forEach(jean => {
+    const {src,nombre,precio,href,boton} = jean
+    girdJeans.innerHTML += `
+    <div class="grid-vestidos__items">
+        <img src=${src} alt="">
+        <h2><span>${nombre}</span><br>${Math.round(Math.random()*precio)}</h2>
+        <a href=${href}>
+            <button>${boton}</button>
+        </a>
+    </div>
+    `
+});
+
+tennis.forEach(ten => {
+    const {src,nombre,precio,href,boton} = ten
+    girdTennis.innerHTML += `
+    <div class="grid-vestidos__items">
+        <img src=${src} alt="">
+        <h2><span>${nombre}</span><br>${Math.round(Math.random()*precio)}</h2>
+        <a href=${href}>
+            <button>${boton}</button>
+        </a>
+    </div>
+    `
+});
+
+zandalias.forEach(zandalia => {
+    const {src,nombre,precio,href,boton} = zandalia
+    girdZandalias.innerHTML += `
+    <div class="grid-vestidos__items">
+        <img src=${src} alt="">
+        <h2><span>${nombre}</span><br>${Math.round(Math.random()*precio)}</h2>
+        <a href=${href}>
+            <button>${boton}</button>
+        </a>
+    </div>
+    `
 });
---
+++ data/bdVestidos.js
@@ -0,0 +1,297 @@
+export  {vestidos, faldas, jeans, tennis, zandalias} 
+const vestidos = [
+    {
+       src: "IMG-NEW/VESTIDOS/VESTIDO1/IMG1.png",
+       nombre: 'vestido1',
+       precio: 100000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+        src: "IMG-NEW/VESTIDOS/VESTIDO1/IMG2.png",
+        nombre: 'vestido2',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO1/IMG3.png",
+        nombre: 'vestido3',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO1/IMG4.png",
+        nombre: 'vestido4',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO2/IMG1.png",
+        nombre: 'vestido5',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO2/IMG2.png",
+        nombre: 'vestido6',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO2/IMG3.png",
+        nombre: 'vestido7',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO2/IMG4.png",
+        nombre: 'vestido8',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO3/IMG1.png",
+        nombre: 'vestido9',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO3/IMG2.png",
+        nombre: 'vestido10',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO3/IMG3.png",
+        nombre: 'vestido11',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO3/IMG4.png",
+        nombre: 'vestido12',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO4/IMG1.png",
+        nombre: 'vestido13',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO4/IMG2.png",
+        nombre: 'vestido14',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO4/IMG3.png",
+        nombre: 'vestido15',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+     {
+        src: "IMG-NEW/VESTIDOS/VESTIDO4/IMG4.png",
+        nombre: 'vestido16',
+        precio: 100000,
+        href: "https://wa.me/573126505633",
+        boton: 'WhatsApp'
+     },
+]
+
+const faldas = [
+   {
+      src: "IMG-NEW/FALDAS/FALDA_NEGRA1/IMG1.png",
+      nombre: 'falda1',
+      precio: 100000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+       src: "IMG-NEW/FALDAS/FALDA_NEGRA1/IMG2.png",
+       nombre: 'falda2',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/FALDAS/FALDA_NEGRA1/IMG3.png",
+       nombre: 'falda3',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/FALDAS/FALDA_NEGRA2/IMG1.png",
+       nombre: 'falda4',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/FALDAS/FALDA_NEGRA2/IMG2.png",
+       nombre: 'falda5',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/FALDAS/FALDA_NEGRA2/IMG3.png",
+       nombre: 'falda6',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    }
+    
+]
+
+const jeans = [
+   {
+      src: "IMG-NEW/JEANS/JEANS1/IMG1.png",
+      nombre: 'jean1',
+      precio: 100000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+       src: "IMG-NEW/JEANS/JEANS1/IMG2.png",
+       nombre: 'jean2',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS1/IMG3.png",
+       nombre: 'jean3',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS2/IMG1.png",
+       nombre: 'jean4',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS2/IMG2.png",
+       nombre: 'jean5',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS2/IMG3.png",
+       nombre: 'jean6',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+      src: "IMG-NEW/JEANS/JEANS3/IMG1.png",
+      nombre: 'jean7',
+      precio: 100000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+       src: "IMG-NEW/JEANS/JEANS3/IMG2.png",
+       nombre: 'jean8',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS3/IMG3.png",
+       nombre: 'jean9',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS4/IMG1.png",
+       nombre: 'jean10',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS4/IMG2.png",
+       nombre: 'jean11',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    },
+    {
+       src: "IMG-NEW/JEANS/JEANS4/IMG3.png",
+       nombre: 'jean12',
+       precio: 60000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    }    
+]
+
+const tennis = [
+   {
+      src: "IMG-NEW/CALZADO/TENNIS1/IMG1.png",
+      nombre: 'tennis1',
+      precio: 150000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+       src: "IMG-NEW/CALZADO/TENNIS2/IMG2.png",
+       nombre: 'tennis2',
+       precio: 150000,
+       href: "https://wa.me/573126505633",
+       boton: 'WhatsApp'
+    }  
+]
+
+const zandalias = [
+   {
+      src: "IMG-NEW/CALZADO/ZAPATO1/IMG2.png",
+      nombre: 'zandalia1',
+      precio: 120000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+      src: "IMG-NEW/CALZADO/ZAPATO2/IMG1.png",
+      nombre: 'zandalia2',
+      precio: 120000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+      src: "IMG-NEW/CALZADO/ZANDALIA/IMG1.png",
+      nombre: 'zandalia3',
+      precio: 120000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   },
+   {
+      src: "IMG-NEW/CALZADO/ZANDALIA/IMG2.png",
+      nombre: 'zandalia4',
+      precio: 120000,
+      href: "https://wa.me/573126505633",
+      boton: 'WhatsApp'
+   }  
+]
+
